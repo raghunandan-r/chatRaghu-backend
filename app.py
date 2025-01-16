00b61957a32ea965a6509536b6c0b5f281972a1b
Raghunandan <raghunandan092@gmail.com>
2025-01-15 23:31:18 -0500

    railway build fail fix1

--- app.py
+++ app.py
@@ -490,15 +490,16 @@ async def chat(
 async def test_cors():
     return {"message": "CORS is working"}
 
-## comment out for production
-#
-#if __name__ == "__main__":
-#    import uvicorn
-#    uvicorn.run(
-#        "main:app",
-#        host="127.0.0.1",
-#        port=8080,
-#        reload=True,
-#        log_level="info"
-#    )
-#
+# comment out for production
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(
+#         "main:app",
+#         host="127.0.0.1",
+#         port=8080,
+#         reload=True,
+#         log_level="info"
+#     )
+# 
+# 
